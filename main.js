db08760b4c6843a67c55d012be308c95dedc9b84
Hyân Bringas <badluck4u22@hotmail.com>
2021-03-10 05:06:54 -0600

    added two new projects to portfolio section

--- main.js
+++ main.js
@@ -339,7 +339,7 @@ window.addEventListener("load", () => {
     document.querySelector(".preloader").classList.add("fade-out");
     setTimeout(() => {
         document.querySelector(".preloader").style.display="none";
-    }, 600)
+    }, 300)
 })
 
 //Toggle Style Switcher...
